3fdcdff7eae50d9a1a0eef86f8e4311ab22e49cf
ahnhuiwon <huiwonan882@gmail.com>
2022-05-06 15:50:22 +0900

    3번째 문제 수정본

--- src/stage/ThirdPage.js
+++ src/stage/ThirdPage.js
@@ -8,16 +8,26 @@ const ThirdPage = () => {
     <>
       <Header />
       <Table />
-      <div className="contents_wrap">
+      <div className="third_contents_wrap">
         <p>First time I saw you</p>
+
+        <p>(처음 당신을 본)</p>
 
         <p>At the restaurant</p>
 
+        <p>(그 식당에서)</p>
+
         <p>Let me have a crush on you</p>
 
-        <p>Show me more...</p>
+        <p>(난 그대에게 빠졌습니다.)</p>
 
-        <p>Especially your beautiful face</p>
+        <p>Show me more</p>
+
+        <p>(더 보여주세요)</p>
+
+        <p>Especially your beautiful face...</p>
+
+        <p>(특히 당신의 아름다운 얼굴을...)</p>
       </div>
       <BtnWrap />
     </>
